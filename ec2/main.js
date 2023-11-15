022ad98cbaaef6a62171210c1fe50350d37cf11d
Weslen Guerreiro <savi2w@icloud.com>
2023-11-15 13:27:28 -0300

    feat: Add emojis, I think

--- ec2/main.js
+++ ec2/main.js
@@ -23,22 +23,34 @@ client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+const _$x = async (message, counter = 0, times = 1) => {
+  if (counter === times) return;
+
+  try {
+    const commentImage = await getComment();
+    const imageBuffer = Buffer.from(commentImage, "base64");
+
+    const attachment = new AttachmentBuilder(imageBuffer, {
+      name: "image.png",
+    });
+
+    await message.channel.send({ files: [attachment] });
+
+    return _$x(message, counter + 1, times);
+  } catch (err) {
+    console.error(err);
+
+    return _$x(message, counter, times);
+  }
+};
+
 client.on(Events.MessageCreate, async (message) => {
-  if (message.content === "$x") {
-    try {
-      const commentImage = await getComment();
-      const imageBuffer = Buffer.from(commentImage, "base64");
+  const context = { command: message.content };
+  const pattern = /\$x\$[0-9]/g;
 
-      const attachment = new AttachmentBuilder(imageBuffer, {
-        name: "image.png",
-      });
-
-      await message.channel.send({ files: [attachment] });
-    } catch (err) {
-      console.error(err);
-
-      await message.reply("Alguma coisa deu errado 😔");
-    }
+  if (message.content === "$x") context.command = "$x$1";
+  if (pattern.test(context.command)) {
+    await _$x(message, 0, parseInt(context.command.split("$x$")[1]));
   }
 });
 
